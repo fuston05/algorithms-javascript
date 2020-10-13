d87693cbcf1cf7f6c66a9720a89a4631d172ba31
scott fuston <fuston05@yahoo.com>
2020-10-12 21:05:03 -0400

    2nd pass on ex-5, using filter

--- ex-5.js
+++ ex-5.js
@@ -1,14 +1,20 @@
 // TASK: Implement linear search.
 
 function linearSearch(list, item) {
-  res = null;
+  // *** for loop method ***
+  // res = null;
+  // for (let i = 0; i < list.length; i++) {
+  //   if (list[i] === item) {
+  //     return list[i];
+  //   }
+  // }
+  // return "not found";
 
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === item) {
-      return list[i];
-    }
-  }
-  return "not found";
+  // *** filter method ***
+  res= list.filter(ele => {
+    return ele === item;
+  })
+  return res.length ? res : 'not found';
 }
 
 console.log(linearSearch([2, 6, 7, 90, 103], 90));
