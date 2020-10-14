8df8292f0e685f538fb9fb0ba3cc8d66c45bd942
scott fuston <fuston05@yahoo.com>
2020-10-13 21:41:30 -0400

    deleted so I can start over for extra practice

--- ex-8.js
+++ ex-8.js
@@ -4,36 +4,12 @@
 // protip: compare the arrays item by item and return the concatenated result
 
 const mergeSort= (arr) => {
-  // base case
-  if(arr.length === 1){
-    return arr;
-  }
-
-  const mid = Math.floor(arr.length/2);
-  const left= arr.slice(0, mid);
-  const right= arr.slice(mid);
-  const leftSorted= mergeSort(left);
-  const rightSorted= mergeSort(right);
-
-  return merge(leftSorted, rightSorted);
+  
 
 }
 
 const merge= (left, right) => {
-  const res= [];
-  let indexR= 0;
-  let indexL= 0;
-
-  while(indexL < left.length && indexR < right.length){
-    if(left[indexL] < right[indexR]){
-      res.push(left[indexL]);
-      indexL++;
-    }else{
-      res.push(right[indexR]);
-      indexR++;
-    }
-  }
-  return res.concat(left.slice(indexL)).concat(right.slice(indexR));
+  
 }
 
 
