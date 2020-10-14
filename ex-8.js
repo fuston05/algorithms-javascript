38428500bad343f3f14b8e64bfd93ce818d44750
scott fuston <fuston05@yahoo.com>
2020-10-13 21:51:13 -0400

    re-implements mergeSort again

--- ex-8.js
+++ ex-8.js
@@ -4,12 +4,34 @@
 // protip: compare the arrays item by item and return the concatenated result
 
 const mergeSort= (arr) => {
-  
+  if(arr.length === 1){return arr;}
 
+  const mid= Math.floor(arr.length / 2);
+
+  const left= arr.slice(0, mid);
+  const right= arr.slice(mid);
+
+  const sortedLeft= mergeSort(left);
+  const sortedRight= mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
 }
 
 const merge= (left, right) => {
-  
+  const res= [];
+  let rightInd= 0;
+  let leftInd= 0;
+
+  while(leftInd < left.length && rightInd < right.length){
+    if( left[leftInd] < right[rightInd] ){
+      res.push(left[leftInd]);
+      leftInd++;
+    }else{
+      res.push(right[rightInd]);
+      rightInd++;
+    }
+  }
+  return res.concat(left.slice(leftInd)).concat(right.slice(rightInd));
 }
 
 
